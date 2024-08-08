9eea4e259660201171f25260beea1a7c0127ac07
Mark Saville <mark.saville@outlook.com>
2024-08-08 20:30:45 +0200

    Add Header component

---
+++ src/client/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-blue-600 text-white p-6">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-2">My Travel App</h1>
+        <h6 className="text-lg font-light">
+          The 3 W&apos;s of travelling: Where, When and Weather
+        </h6>
+      </div>
+    </header>
+  );
+}
