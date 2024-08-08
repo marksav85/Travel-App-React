52b12a4a639cf86e0c6305840039fe5713abe7de
Mark Saville <mark.saville@outlook.com>
2024-08-08 20:31:36 +0200

    Create WeatherContextProvider for managing weather data

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { WeatherProvider } from "./context/WeatherContextProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <WeatherProvider>
     <App />
-  </React.StrictMode>,
-)
+  </WeatherProvider>
+);
