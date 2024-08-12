5f90236efecce8f8acf308cb6ad4248e3b00198b
Mark Saville <mark.saville@outlook.com>
2024-08-12 18:18:53 +0200

    Update Weather component with new icons and styles

--- src/client/components/weather/Weather.jsx
+++ src/client/components/weather/Weather.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import WeatherContext from "../../context/WeatherContextProvider";
 import Lottie from "lottie-react";
 import travelAnimation from "../../../assets/travel-animation.json";
+import raindropIcon from "/media/weather-icons/raindrop.svg";
+import cloudyIcon from "/media/weather-icons/cloudy.svg";
 
 const formatDate = (dateString) => {
   // Parse the date string into a Date object
@@ -16,13 +18,8 @@ const formatDate = (dateString) => {
 };
 
 const Weather = () => {
-  const {
-    city,
-    temperatureCelsius,
-    weatherData,
-    weatherCardStyle,
-    weatherIcon,
-  } = useContext(WeatherContext);
+  const { city, temperatureCelsius, weatherData, tempIcon, weatherIcon } =
+    useContext(WeatherContext);
 
   if (!weatherData) {
     return (
@@ -40,57 +37,92 @@ const Weather = () => {
   }
 
   return (
-    <div
-      style={weatherCardStyle}
-      className="bg-white shadow-lg rounded-lg w-full max-w-md mx-auto"
-    >
-      <div id="weather-card" className="flex flex-col">
-        <div id="weather-header" className=" text-white p-4 rounded-t-lg mb-4">
-          <h1 className="text-xl font-semibold text-center">{city}</h1>
+    <div className="bg-white shadow-lg rounded-lg w-full max-w-md mx-auto">
+      <div id="weather-card" className="flex flex-col h-full">
+        <div
+          id="weather-header"
+          className="bg-blue-500 text-white rounded-t-lg flex-shrink-0 
+                   p-2 md:p-4 lg:p-6"
+        >
+          <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-center">
+            {city}
+          </h2>
         </div>
-        <div id="weather-card-body" className="p-6">
-          <div id="weather-details" className="space-y-4">
-            <div
-              id="weather-date"
-              className="weather-data text-lg text-gray-600"
-            >
-              {formatDate(weatherData.date)}
-            </div>
-            <div
-              id="weather-temperature"
-              className="weather-data text-2xl font-semibold"
-            >
-              {temperatureCelsius}°C
-            </div>
-            <div
-              id="weather-clouds"
-              className="weather-data text-lg text-gray-600"
-            >
-              Clouds: {weatherData.cloud_cover.afternoon}%
-            </div>
-            <div
-              id="weather-rainfall"
-              className="weather-data text-lg text-gray-600"
-            >
-              Rainfall: {weatherData.precipitation.total} mm
-            </div>
-            <div id="weather-icon" className="weather-data mb-4">
-              <img
-                src={weatherIcon}
-                alt="Weather icon"
-                className="w-20 h-20 mx-auto"
-              />
+        <div className="flex flex-col flex-grow">
+          <div
+            id="weather-card-body"
+            className="flex-1 h-full p-6 text-blue-500 font-semibold"
+          >
+            <div id="weather-details" className="space-y-4">
+              <div className="flex flex-col items-start">
+                <div
+                  id="weather-date"
+                  className="weather-data flex justify-start text-lg md:text-xl lg:text-2x"
+                >
+                  <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-center">
+                    {formatDate(weatherData.date)}
+                  </h2>
+                </div>
+              </div>
+              <div>
+                <div className="flex flex-grow">
+                  <div className="flex-1 text-lg md:text-xl lg:text-2xl">
+                    <img src={cloudyIcon} className="w-20 h-20" alt="" />
+                  </div>
+                  <div
+                    id="weather-clouds"
+                    className="weather-data flex items-center flex-1 text-lg md:text-xl lg:text-2xl"
+                  >
+                    {weatherData.cloud_cover.afternoon}%
+                  </div>
+                </div>
+                <div className="flex flex-grow">
+                  <div className="flex-1 text-lg md:text-xl lg:text-2xl">
+                    <img src={raindropIcon} className="w-20 h-20" alt="" />
+                  </div>
+                  <div
+                    id="weather-rainfall"
+                    className="weather-data flex items-center flex-1 text-lg md:text-xl lg:text-2xl"
+                  >
+                    {weatherData.precipitation.total} mm
+                  </div>
+                </div>
+                <div className="flex flex-grow">
+                  <div className="flex-1 text-lg md:text-xl lg:text-2xl">
+                    <img src={tempIcon} className="w-20 h-20" alt="" />
+                  </div>
+                  <div
+                    id="weather-rainfall"
+                    className="weather-data flex items-center flex-1 text-lg md:text-xl lg:text-2xl"
+                  >
+                    {temperatureCelsius}°C
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col flex-grow">
+                <div
+                  id="weather-icon"
+                  className="weather-data flex flex-col flex-1 items-center justify-center mb-4"
+                >
+                  <img
+                    src={weatherIcon}
+                    alt="Weather icon"
+                    className="w-20 h-20 mx-auto"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          id="weather-footer"
-          className="bg-gray-100 p-4 rounded-b-lg text-center text-sm text-gray-600"
-        >
-          <p id="weather-footer-text">
-            * Temperature, cloud, and rainfall are measured as averages
-            throughout the day
-          </p>
+
+          <div
+            id="weather-footer"
+            className="bg-gray-100 p-4 rounded-b-lg text-center text-sm text-gray-600"
+          >
+            <p id="weather-footer-text">
+              * Temperature, cloud, and rainfall are measured as averages
+              throughout the day
+            </p>
+          </div>
         </div>
       </div>
     </div>
