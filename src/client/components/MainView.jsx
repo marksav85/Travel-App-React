5d185c3ae8f116428f8bbe10ff5f4d5f523264f0
Mark Saville <mark.saville@outlook.com>
2024-08-16 14:30:57 +0200

    Update dependencies and remove unused code

--- src/client/components/MainView.jsx
+++ src/client/components/MainView.jsx
@@ -1,15 +1,29 @@
+// src/client/components/MainView.js
+import { useRef, useEffect, useContext } from "react";
+import WeatherContext from "../context/WeatherContextProvider";
 import Destination from "./destination/Destination";
 import Weather from "./weather/Weather";
 import travelCamper from "/media/travel_camper.jpg";
 
 export default function MainView() {
+  const { submitted } = useContext(WeatherContext); // Access the submitted state from context
+  const weatherRef = useRef(null); // Create a ref for the Weather component
+
+  useEffect(() => {
+    if (submitted && weatherRef.current) {
+      weatherRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [submitted]);
+
   return (
     <div
       className="flex flex-col md:flex-row w-full h-full justify-center items-center space-y-6 md:space-y-0 md:space-x-6 p-6 bg-cover bg-center"
       style={{ backgroundImage: `url(${travelCamper})` }}
     >
       <Destination />
-      <Weather />
+      <div className="w-full max-w-md" ref={weatherRef}>
+        <Weather />
+      </div>
     </div>
   );
 }
